9133da7362bd58b857c18e5bdb6467d6d6678433
Frederic Werner <frederic@werner-net.work>
2018-08-10 11:49:03 +0200

    Add comments and author, version

--- check_dht.py
+++ check_dht.py
@@ -1,14 +1,20 @@
+#This plugin will read the temperature and humidity values from your sensor (dht11, dht22, 3202) and return it in readable format for tools like icinga oder nagios including perfdata for visualization.
+#This check plugin needs the adafruit dht library available on: https://github.com/adafruit/Adafruit_Python_DHT.git
+
 import sys
 import argparse
 import Adafruit_DHT
 
+AUTHOR = "Frederic Werner"
+VERSION = 1.0
+
 parser = argparse.ArgumentParser()
-parser.add_argument("model", help="the model you use [11|22|3202]", type=int, choices=[11, 22, 3202])
+parser.add_argument("model", help="the sensor model you use [11|22|3202]", type=int, choices=[11, 22, 3202])
 parser.add_argument("gpio", help="the gpio pin number you are using", type=int)
-parser.add_argument("-wt", help="warning for temperature", type=int)
-parser.add_argument("-ct", help="critical for temperature", type=int)
-parser.add_argument("-wh", help="warning for humidity", type=int)
-parser.add_argument("-ch", help="warning for humidity", type=int)
+parser.add_argument("-wt", help="warning value for temperature", type=int)
+parser.add_argument("-ct", help="critical value for temperature", type=int)
+parser.add_argument("-wh", help="warning value for humidity", type=int)
+parser.add_argument("-ch", help="warning value for humidity", type=int)
 args = parser.parse_args()
 
 model = args.model
